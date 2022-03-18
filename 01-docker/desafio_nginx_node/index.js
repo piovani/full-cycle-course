f349fa2450d5cba6283d6fb65534c9e781094eea
Allison Piovani <allison.piovani@APiovani-Mac.local>
2022-03-17 22:19:54 -0300

    Finished challenge nginx with node

--- 01-docker/desafio_nginx_node/index.js
+++ 01-docker/desafio_nginx_node/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 const config = {
     host: 'db',
     user: 'root',
@@ -10,15 +10,26 @@ const config = {
 const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) values('Allison')`
+let sql = `CREATE TABLE nomes (nome varchar(255) NULL)
+    ENGINE=InnoDB
+    DEFAULT CHARSET=latin1
+    COLLATE=latin1_swedish_ci;
+`;
 connection.query(sql)
+
+
 connection.end()
 
-
 app.get('/', (req,res) => {
+    const connection = mysql.createConnection(config)
+    
+    sql = `INSERT INTO people(name) values('Allison')`
+    
+    connection.query(sql)
+    
     res.send('<h1>Full Cycle</h1>')
 })
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log('Rodando na porta ' + port)
 })
